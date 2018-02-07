61a4067414c7603fa2188ca52687b8517d86b919
putrimu <putrimariahulfa@gmail.com>
2018-02-07 16:39:17 +0700

    Initial commit

---
+++ app/src/main/java/mu/android/games/catchgames/MainActivity.java
@@ -0,0 +1,24 @@
+package mu.android.games.catchgames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv_11, iv_12, iv_13,
+            iv_21, iv_22, iv_23,
+            iv_31, iv_32, iv_33,
+            iv_41, iv_42, iv_43,
+            iv_51, iv_52, iv_53;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
